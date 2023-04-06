875b386987109a8880b1656583d12fa4c83e4013
Divinefavour Possible <possibledivinefavour@gmail.com>
2023-04-06 02:00:01 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
-**/
+ * return: void
+*/
 
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
-
 	_putchar('\n');
 }
